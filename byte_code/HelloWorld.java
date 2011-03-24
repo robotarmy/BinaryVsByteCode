7041426e2d18fe4dacf3bb5e1a82c57481a07520
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2011-03-24 13:46:39 -0700

    heya!

---
+++ byte_code/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+      public static void main(String[] args) {
+          String hello = new String("Hello World");
+          System.out.println(hello);
+      }
+
+}
+
